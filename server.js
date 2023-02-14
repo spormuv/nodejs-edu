3c2ff75d443b2644aea4b4209f32fabcf903104f
Mike <spormuv@gmail.com>
2023-02-14 21:20:21 +0300

    init: add starting files

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
+
+app.listen(PORT, (err) => {
+  err ? console.error(err) : console.log(`Listening port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(createPath('index'));
+});
+
+app.get('/contacts', (req, res) => {
+  res.sendFile(createPath('contacts'));
+});
+
+app.get('/about-us', (req, res) => {
+  res.redirect('/contacts');
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(createPath('error'));
+});
