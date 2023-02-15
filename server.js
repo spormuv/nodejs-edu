3a5331b6c3ff3f6bf1c69ee8cadd8d6d57e6c37e
Mike <spormuv@gmail.com>
2023-02-15 23:27:39 +0300

    feat: implement post query processing

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms')
 );
 
+app.use(express.urlencoded({ extended: false }));
+
 app.use(express.static('styles'));
 
 // express server response
@@ -44,12 +46,40 @@ app.get('/contacts', (req, res) => {
 
 app.get('/posts/:id', (req, res) => {
   const title = 'Post';
-  res.render(createPath('post'), { title });
+  const post = {
+    id: '1',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quidem provident, dolores, vero laboriosam nemo mollitia impedit unde fugit sint eveniet, minima odio ipsum sed recusandae aut iste aspernatur dolorem.',
+    title: 'Post title',
+    date: '05.05.2021',
+    author: 'Yauhen',
+  };
+  res.render(createPath('post'), { title, post });
 });
 
 app.get('/posts', (req, res) => {
   const title = 'Posts';
-  res.render(createPath('posts'), { title });
+  const posts = [
+    {
+      id: '1',
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quidem provident, dolores, vero laboriosam nemo mollitia impedit unde fugit sint eveniet, minima odio ipsum sed recusandae aut iste aspernatur dolorem.',
+      title: 'Post title',
+      date: '05.05.2021',
+      author: 'Yauhen',
+    },
+  ];
+  res.render(createPath('posts'), { title, posts });
+});
+
+app.post('/add-post', (req, res) => {
+  const { title, author, text } = req.body;
+  const post = {
+    id: new Date(),
+    date: new Date().toLocaleDateString(),
+    title,
+    author,
+    text,
+  };
+  res.render(createPath('post'), { post, title });
 });
 
 app.get('/add-post', (req, res) => {
