2ec71752129fe730c87a13d9b90b1ab326968f09
Mike <spormuv@gmail.com>
2023-02-15 14:38:19 +0300

    feat: implement ejs templates

--- server.js
+++ server.js
@@ -1,26 +1,52 @@
+// import & variables
 const express = require('express');
 const path = require('path');
-
-const app = express();
 const PORT = 3000;
-const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
 
+// express
+const app = express();
+app.set('view engine', 'ejs');
+
+// functions
+const createPath = (page) =>
+  path.resolve(__dirname, 'ejs-views', `${page}.ejs`);
+
+// server functioning
 app.listen(PORT, (err) => {
   err ? console.error(err) : console.log(`Listening port ${PORT}`);
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(createPath('index'));
+  const title = 'Home';
+  res.render(createPath('index'), { title });
 });
 
 app.get('/contacts', (req, res) => {
-  res.sendFile(createPath('contacts'));
+  const title = 'Contacts';
+  const contacts = [
+    { name: 'YouTube', link: 'http://youtube.com/YauhenKavalchuk' },
+    { name: 'Twitter', link: 'http://github.com/YauhenKavalchuk' },
+    { name: 'GitHub', link: 'http://twitter.com/YauhenKavalchuk' },
+  ];
+  res.render(createPath('contacts'), { contacts, title });
 });
 
-app.get('/about-us', (req, res) => {
-  res.redirect('/contacts');
+app.get('/posts/:id', (req, res) => {
+  const title = 'Post';
+  res.render(createPath('post'), { title });
+});
+
+app.get('/posts', (req, res) => {
+  const title = 'Posts';
+  res.render(createPath('posts'), { title });
+});
+
+app.get('/add-post', (req, res) => {
+  const title = 'Add post';
+  res.render(createPath('add-post'), { title });
 });
 
 app.use((req, res) => {
-  res.status(404).sendFile(createPath('error'));
+  const title = 'Error Page';
+  res.status(404).render(createPath('error'), { title });
 });
